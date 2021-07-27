597ae0a7be9976f4f5ccfc4c5c98384dff37380c
Jeff G <jeff.glaister@gmail.com>
2021-07-27 14:14:32 -0400

    Changed default output name

--- nipypeVBM/interfaces.py
+++ nipypeVBM/interfaces.py
@@ -7,8 +7,7 @@ import nipype.utils.filemanip as fip
 class GenerateTemplateInputSpec(base.BaseInterfaceInputSpec):
     input_file = base.File(exists=True, desc='input image', mandatory=True)
     flip_axis = base.traits.Int(desc='Axis number to flip (-1 to not flip)', default_value=1)
-    output_name = base.traits.Str(desc='Filename for output template', default_value='template')
-
+    output_name = base.traits.Str(desc='Filename for output template')
 
 class GenerateTemplateOutputSpec(base.TraitedSpec):
     template_file = base.File(exists=True, desc='output template')
@@ -31,13 +30,20 @@ class GenerateTemplate(base.BaseInterface):
             template_data = (np.average(vol_data, axis=-1) + np.average(np.flip(vol_data, axis=1), axis=-1)) / 2
 
         template_obj = nib.Nifti1Image(template_data, vol_obj.affine, vol_obj.header)
-        template_obj.to_filename(self.inputs.output_name + '.nii.gz')
+        if base.isdefined(self.inputs.output_name):
+            output_name = self.inputs.output_name + '.nii.gz'
+        else:
+            output_name = 'template.nii.gz'
+        template_obj.to_filename(output_name)
 
         return runtime
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        outputs['template_file'] = os.path.abspath(self.inputs.output_name + '.nii.gz')
+        if base.isdefined(self.inputs.output_name):
+            outputs['template_file'] = os.path.abspath(self.inputs.output_name + '.nii.gz')
+        else:
+            outputs['template_file'] = os.path.abspath('template.nii.gz')
         return outputs
 
 
